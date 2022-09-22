8575461e07f7718560f041c61938590fd69501ac
CVHoang0205 <106986621+CVHoang0205@users.noreply.github.com>
2022-09-22 17:19:26 +0700

    a

--- session1.js
+++ session1.js
@@ -1,6 +1,6 @@
 const { appendFile } = require("fs");
 const http = require("http");
-const port = 5000;
+const port = process.env.PORT || 3000;
 
  const app = http.createServer((req, res) => {
   res.write("");
